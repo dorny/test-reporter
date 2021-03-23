3a0bb833dcd98e51d794617e270d642d55cde403
Michal Dorner <dorner.michal@gmail.com>
2021-03-23 21:39:47 +0100

    #83 - Use non-capturing optional group, add tests and update dist

--- src/utils/parse-utils.ts
+++ src/utils/parse-utils.ts
@@ -1,6 +1,5 @@
 export function parseNetDuration(str: string): number {
-  // matches dotnet duration: 00:00:00.0010000
-  const durationRe = /^(\d\d):(\d\d):(\d\d($|\.\d+$))/
+  const durationRe = /^(\d\d):(\d\d):(\d\d(?:\.\d+)?)$/
   const durationMatch = str.match(durationRe)
   if (durationMatch === null) {
     throw new Error(`Invalid format: "${str}" is not NET duration`)
