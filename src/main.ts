ff6e1f225bc3078d5ad19d0dc5ca0dbb22e83b64
luisito666 <luispenagos91@gmail.com>
2022-10-04 08:40:31 -0500

    Add extra outputs

--- src/main.ts
+++ src/main.ts
@@ -193,6 +193,8 @@ class TestReporter {
     core.info(`Check run create response: ${resp.status}`)
     core.info(`Check run URL: ${resp.data.url}`)
     core.info(`Check run HTML: ${resp.data.html_url}`)
+    core.setOutput('url', resp.data.url)
+    core.setOutput('url_html', resp.data.html_url)
 
     return results
   }
