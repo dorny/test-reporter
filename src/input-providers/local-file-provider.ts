24bbe17d844d09bb0da42930f7b822516b65e700
Martin Fillafer <unki2aut@gmail.com>
2024-07-01 16:23:25 +0200

    lint: fix linting issues

--- src/input-providers/local-file-provider.ts
+++ src/input-providers/local-file-provider.ts
@@ -4,10 +4,7 @@ import {FileContent, InputProvider, ReportInput} from './input-provider'
 import {listFiles} from '../utils/git'
 
 export class LocalFileProvider implements InputProvider {
-  constructor(
-    readonly name: string,
-    readonly pattern: string[]
-  ) {}
+  constructor(readonly name: string, readonly pattern: string[]) {}
 
   async load(): Promise<ReportInput> {
     const result: FileContent[] = []
