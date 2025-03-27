3d6255d50f67321456bf0714841dc98ffa41fa03
JulianRooze <julian@newblack.io>
2025-03-27 22:56:23 +0100

    .

--- dist/index.js
+++ dist/index.js
@@ -55,7 +55,6 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.LocalFileProvider = void 0;
 const fs = __importStar(__nccwpck_require__(9896));
 const fast_glob_1 = __importDefault(__nccwpck_require__(5648));
-const git_1 = __nccwpck_require__(5454);
 const adm_zip_1 = __importDefault(__nccwpck_require__(1316));
 const path_1 = __importDefault(__nccwpck_require__(6928));
 class LocalFileProvider {
@@ -86,7 +85,7 @@ class LocalFileProvider {
     }
     listTrackedFiles() {
         return __awaiter(this, void 0, void 0, function* () {
-            return (0, git_1.listFiles)();
+            return [];
         });
     }
 }
@@ -1134,81 +1133,6 @@ class TestCaseResult {
     }
 }
 exports.TestCaseResult = TestCaseResult;
-
-
-/***/ }),
-
-/***/ 5454:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.listFiles = listFiles;
-const core = __importStar(__nccwpck_require__(7484));
-const exec_1 = __nccwpck_require__(5236);
-function listFiles() {
-    return __awaiter(this, void 0, void 0, function* () {
-        core.startGroup('Listing all files tracked by git');
-        let output = '';
-        try {
-            output = (yield (0, exec_1.getExecOutput)('git', ['ls-files', '-z'])).stdout;
-        }
-        finally {
-            fixStdOutNullTermination();
-            core.endGroup();
-        }
-        return output.split('\u0000').filter(s => s.length > 0);
-    });
-}
-function fixStdOutNullTermination() {
-    // Previous command uses NULL as delimiters and output is printed to stdout.
-    // We have to make sure next thing written to stdout will start on new line.
-    // Otherwise things like ::set-output wouldn't work.
-    core.info('');
-}
 
 
 /***/ }),
--- src/utils/git.ts
+++ src/utils/git.ts
@@ -2,16 +2,7 @@ import * as core from '@actions/core'
 import {getExecOutput} from '@actions/exec'
 
 export async function listFiles(): Promise<string[]> {
-  core.startGroup('Listing all files tracked by git')
-  let output = ''
-  try {
-    output = (await getExecOutput('git', ['ls-files', '-z'])).stdout
-  } finally {
-    fixStdOutNullTermination()
-    core.endGroup()
-  }
-
-  return output.split('\u0000').filter(s => s.length > 0)
+  return []
 }
 
 function fixStdOutNullTermination(): void {
