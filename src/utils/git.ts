67e7f6effb71d43ed0e5427481a6bcbc13e8ef1a
JulianRooze <julian@newblack.io>
2025-03-27 22:59:40 +0100

    .

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,13 +1,3 @@
-import * as core from '@actions/core'
-import {getExecOutput} from '@actions/exec'
-
 export async function listFiles(): Promise<string[]> {
   return []
-}
-
-function fixStdOutNullTermination(): void {
-  // Previous command uses NULL as delimiters and output is printed to stdout.
-  // We have to make sure next thing written to stdout will start on new line.
-  // Otherwise things like ::set-output wouldn't work.
-  core.info('')
 }
