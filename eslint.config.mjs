d788b88f5413d356734fe8c9655482b33d9f159a
Jozef Izso <izso@cisco.com>
2025-06-07 15:47:32 +0200

    Use `jest` with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import github from 'eslint-plugin-github'
+import jest from 'eslint-plugin-jest'
 
 export default [
   github.getFlatConfigs().recommended,
@@ -16,11 +17,25 @@ export default [
     },
   },
   {
+    files: ['__tests__/**/*.test.ts'],
+    ...jest.configs['flat/recommended'],
+    plugins: {
+      jest,
+    },
+    languageOptions: {
+      globals: jest.environments.globals.globals,
+    },
+    rules: {
+      'i18n-text/no-en': 'off',
+      'import/no-namespace': 'off',
+      "@typescript-eslint/array-type": ['error', {default: 'array'}],
+    },
+  },
+  {
     ignores: [
       'dist/**',
       'lib/**',
       'node_modules/**',
-      '__tests__/**',
       '__tests__/__snapshots__/**',
       '__tests__/__results__/**',
       'assets/**',
