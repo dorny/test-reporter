6a822cbbbe12a1e3a097a7e77dcb977c7afdb6c3
Sarah Smith <sez@storybridge.org>
2026-09-11 21:28:09 +1000

    Move fakes to test

--- __tests__/unreal-json.test.ts
+++ __tests__/unreal-json.test.ts
@@ -12,19 +12,22 @@ import {
   TestPathsMapElement,
   UnrealJsonParser
 } from '../src/parsers/unreal-engine/unreal-json-parser.js'
-import {
-  aReport,
-  FakeSingleUnrealTest,
-  UnrealEngineTestExample,
-  UnrealReportWithSingleTest
-} from '../src/parsers/unreal-engine/fakes.js'
 import {TestCaseResult, TestExecutionResult, TestGroupResult, TestSuiteResult} from '../src/test-results.js'
-import {UnrealReport} from '../src/parsers/unreal-engine/unreal-json-types.js'
-import {stripVTControlCharacters} from 'node:util'
+import {
+  UnrealDevice,
+  UnrealReport,
+  UnrealTest,
+  UnrealTestEvent,
+  UnrealTestEventTypeRecord
+} from '../src/parsers/unreal-engine/unreal-json-types.js'
 import stripBom from 'strip-bom'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+const aDevice = device()
+const UnrealReportWithSingleTest = unrealReportWithSingleTest()
+const FakeSingleUnrealTest = JSON.stringify(UnrealReportWithSingleTest)
 
 function getElementData(root: TestPathsMapElement, ix: number) {
   const el = root.childElements[ix]
@@ -222,3 +225,189 @@ describe('UnrealJsonParser', () => {
     expect(result.time).toStrictEqual(testData.totalDuration)
   })
 })
+
+function eventTypeRecord(): UnrealTestEventTypeRecord {
+  return {
+    type: 'Error',
+    message: 'LogDialog: Your test failed',
+    context: 'log',
+    artifact: '00000000000000000000000000000000'
+  }
+}
+
+function entry(): UnrealTestEvent {
+  return {
+    event: eventTypeRecord(),
+    filename: 'src/Proj/Dialog.cpp',
+    lineNumber: 63,
+    timestamp: '2026.08.06-11.49.53'
+  }
+}
+
+function device(): UnrealDevice {
+  return {
+    deviceName: 'Cyberdyne-Systems-T2000.local',
+    instance: 'F35C7BB1284BDF8F6E09B880ABB1710E',
+    instanceName: 'Cyberdyne.Term2.local-68471',
+    platform: 'MacEditor',
+    oSVersion: 'macOS 26.5.2 25F84',
+    model: 'Default',
+    gPL: 'Apple M3 Max',
+    cPUModel: 'Apple M3 Max',
+    rAMInGB: 36,
+    renderMode: 'SM6',
+    rHI: '',
+    appInstanceLog: ''
+  }
+}
+
+function unrealReportWithSingleTest(): UnrealReport {
+  const aTest = aTestWithResult('Private.Test.ThisIs.Not.AGroup.Basic')
+  return {
+    devices: [aDevice],
+    reportCreatedOn: '2026.08.10-11.46.00',
+    succeeded: 1,
+    succeededWithWarnings: 0,
+    failed: 0,
+    notRun: 0,
+    inProcess: 0,
+    totalDuration: aTest.duration,
+    comparisonExported: false,
+    comparisonExportDirectory: '',
+    tests: [aTest]
+  }
+}
+
+function aTestWithResult(
+  fullTestPath: string = 'SUITE.PATH.TEST',
+  state: UnrealTest['state'] = 'Success',
+  entries: UnrealTestEvent[] = [entry()]
+): UnrealTest {
+  const aTest: UnrealTest = {
+    testDisplayName: 'Basic',
+    fullTestPath: 'Private.Test.ThisIs.Not.AGroup.Basic',
+    state: 'Success',
+    deviceInstance: ['F35C7BB1284BDF8F6E09B880ABB1710E'],
+    duration: 0.0080692917108535767,
+    dateTime: '2026.08.10-11.45.59',
+    entries: [],
+    warnings: 0,
+    errors: 0,
+    artifacts: []
+  }
+  let testDisplayName = 'DEFAULT_TEST_NAME'
+  const dotIx = fullTestPath.lastIndexOf('.')
+  if (dotIx !== -1) {
+    testDisplayName = fullTestPath.substring(dotIx + 1)
+    if (!testDisplayName || testDisplayName.length === 0) {
+      throw Error('bad full test path - dots must be embedded.in.string.path')
+    }
+  }
+  return {
+    ...aTest,
+    testDisplayName,
+    fullTestPath,
+    state,
+    entries
+  }
+}
+
+function aReport(
+  suiteNames: string[] = ['SUITE'],
+  groupNames: string[][] = [],
+  testsPerGroup: number = 2,
+  testNamePrefix: string = 'Test_'
+): UnrealReport {
+  const tests: UnrealTest[] = []
+  for (let ix = 0; ix < suiteNames.length; ix++) {
+    const suite = suiteNames[ix]
+    if (groupNames.length > ix && groupNames[ix].length >= 1) {
+      for (let jx = 0; jx < groupNames[ix].length; jx++) {
+        for (let kx = 0; kx < testsPerGroup; kx++) {
+          const suffix = testNamePrefix.length > 0 ? `.${testNamePrefix}${kx}` : ''
+          const t = aTestWithResult(`${suite}.${groupNames[ix][jx]}${suffix}`)
+          tests.push(t)
+        }
+      }
+    } else {
+      for (let kx = 0; kx < testsPerGroup; kx++) {
+        const suffix = testNamePrefix.length > 0 ? `.${testNamePrefix}${kx}` : ''
+        const t = aTestWithResult(`${suite}${suffix}`)
+        tests.push(t)
+        if (testNamePrefix.length === 0) {
+          break // pretend testsPerGroup is 1, because there's no test names to differentiate
+        }
+      }
+    }
+  }
+  return {
+    ...UnrealReportWithSingleTest,
+    tests
+  }
+}
+
+/** From Unreal's website: https://dev.epicgames.com/documentation/unreal-engine/review-test-results-in-unreal-engine#json */
+const UnrealEngineTestExample = {
+  devices: [
+    {
+      deviceName: '00-00-000-00',
+      instance: '878B6A854613D3B6A69CDEAFBA1C5DBA',
+      platform: 'WindowsEditor',
+      oSVersion: 'Windows Server 2022 (21H2) [10.0.20348.524] ',
+      model: 'Default',
+      gPU: 'Microsoft Basic Display Adapter',
+      cPUModel: 'Intel(R) Xeon(R) Platinum 8259CL CPU @ 2.50GHz',
+      rAMInGB: 127,
+      renderMode: 'D3D11_SM5',
+      rHI: 'DirectX 11',
+      appInstanceLog: ''
+    }
+  ],
+  reportCreatedOn: '2000.01.01-12.00.00',
+  succeeded: 1,
+  succeededWithWarnings: 0,
+  failed: 0,
+  notRun: 0,
+  inProcess: 0,
+  totalDuration: 0.3,
+  comparisonExported: false,
+  comparisonExportDirectory: '',
+  tests: [
+    {
+      testDisplayName: 'Test1',
+      fullTestPath: 'Project.Functional Tests.SomeGroup.Test1',
+      state: 'Skipped',
+      deviceInstance: ['878B6A854613D3B6A69CDEAFBA1C5DBA'],
+      duration: 0,
+      dateTime: '2000.01.01-12.00.00',
+      entries: [
+        {
+          event: {
+            type: 'Info',
+            message: 'Skipping test: Tests for review [config]',
+            context: '',
+            artifact: '00000000000000000000000000000000'
+          },
+          filename: '',
+          lineNumber: -1,
+          timestamp: '2000.01.01-12.00.00'
+        }
+      ],
+      warnings: 0,
+      errors: 0,
+      artifacts: []
+    },
+    {
+      testDisplayName: 'Test2',
+      fullTestPath: 'Project.Functional Tests.SomeGroup.Test2',
+      state: 'Success',
+      deviceInstance: ['878B6A854613D3B6A69CDEAFBA1C5DBA'],
+      duration: 0.3,
+      dateTime: '2000.01.01-12.00.00',
+      entries: [],
+      warnings: 0,
+      errors: 0,
+      artifacts: []
+    }
+  ]
+}
